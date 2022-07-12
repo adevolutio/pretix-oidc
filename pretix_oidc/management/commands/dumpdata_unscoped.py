2a3af57391231b93e6c7d829aaa7eac16e33de3d
João Polónia <jpolonia@evolutio.pt>
2022-07-12 21:47:32 +0100

    Remove Unscoped management command - not needed anymore with the more general unscoped_manage.py solution

--- pretix_oidc/management/commands/dumpdata_unscoped.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.commands.dumpdata import Command as DumpdataCommand
-from django_scopes import scopes_disabled
-
-
-class Command(DumpdataCommand):
-    def handle(self, *args, **options):
-        with scopes_disabled():
-            return super().handle(*args, **options)
